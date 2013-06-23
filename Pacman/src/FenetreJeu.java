719a70c42dfecc39a544843b94c811cb77fc6076
Yann Rolland <GrosKiff@gmail.com>
2013-06-24 00:13:51 +0200

    pas grand chose

--- Pacman/src/FenetreJeu.java
+++ Pacman/src/FenetreJeu.java
@@ -1,4 +1,9 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class FenetreJeu extends JFrame {
 	public FenetreJeu(){
@@ -10,7 +15,20 @@ public class FenetreJeu extends JFrame {
 		setTitle("Pacman");
 		setSize(600,400);
 		setLocationRelativeTo(null);
-		setResizable(false);
+		setResizable(true);
+		Dimension min = new Dimension(200, 200);
+		Dimension max = new Dimension(500, 700);
+		setMinimumSize(min);
+		setMaximumSize(max);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setContentPane(buildContentPanel());
+	}
+	
+	private JPanel buildContentPanel(){
+		JPanel panel = new JPanel();
+		panel.setLayout(new FlowLayout());
+		panel.setBackground(Color.BLACK);
+				
+		return panel;
 	}
 }
